59650bb3afd64420e55bf8e0a404fc55290c682e
Dmitrii Kostromin <crabik.dk@gmail.com>
2025-11-07 14:29:36 +0300

    Add Duck to animals

---
+++ src/main/java/com/javarush/island/kostromin/entity/organisms/animal/herbivore/Duck.java
@@ -0,0 +1,7 @@
+package com.javarush.island.kostromin.entity.organisms.animal.herbivore;
+
+public class Duck extends Herbivore{
+    public Duck(String name, double weight, int maxPerCell, int speed, double foodNeeded) {
+        super(name, weight, maxPerCell, speed, foodNeeded);
+    }
+}
