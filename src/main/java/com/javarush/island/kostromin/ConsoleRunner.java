c148938279540c0508c68078b264b080eefc3bde
Dmitrii Kostromin <crabik.dk@gmail.com>
2025-11-07 15:03:52 +0300

    main Modified

--- src/main/java/com/javarush/island/kostromin/ConsoleRunner.java
+++ src/main/java/com/javarush/island/kostromin/ConsoleRunner.java
@@ -3,6 +3,5 @@ package com.javarush.island.kostromin;
 public class ConsoleRunner {
     public static void main(String[] args) {
         System.out.println("Start project!");
-        System.out.println("try @1");
     }
 }
